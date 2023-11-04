2855cc3b0afa320e73eb370c4b6f43201c407f3e
tomatoboy <hayatarou.ay@gmail.com>
2023-11-04 16:51:09 +0900

    qrcode reader

--- src/App.js
+++ src/App.js
@@ -6,12 +6,15 @@ import User from './components/User';
 import Home from './components/Home';
 import { AuthProvider } from './context/AuthContext';
 import {Routes ,Route} from 'react-router-dom';
+
+import Reader from './components/QRead';
 function App() {
   return (
     <AuthProvider>
     <div className="App">
-      <Chatbot />
+      <Reader/>
       <Routes>
+        <Route path="/chatbot" element={<Chatbot />} />
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login/>} />
         <Route path="/user" element={<User/>} />
